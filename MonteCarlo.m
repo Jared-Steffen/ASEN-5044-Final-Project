f7280811cb1808096ddd79d79a25b7b0550a0056
Nathan <nawh4475@colorado.edu>
2025-12-09 00:12:08 +0000

    Completed NEES and NIS tests for LKF and EKF, needs verifying. EKF might have problems? It's showing many warnings for singular matrices, causing errors in output.

--- MonteCarlo.m
+++ MonteCarlo.m
@@ -1,6 +1,4 @@
-clc; clear all;
-
-%% Simulate Non-Linear ODEs and Measured Outputs
+clc; clear all; close all;
 
 % Constants
 mu = 398600; % km^3/s^2
@@ -18,6 +16,28 @@ pert_var0 = nom_var0 + delta_x0;
 % Simulation time
 delta_t = 10; %s
 tspan = 0:delta_t:14000; % s
+N = length(tspan);
+
+% Initialize Covariance
+Pp0 = diag([1000,1,1000,1]);
+
+% ode45 options
+options = odeset('RelTol',1e-6,'AbsTol',1e-9);
+
+% Loading Qtrue and Rtrue
+data = load('orbitdeterm_finalproj_KFdata.mat');
+Qtrue = data.Qtrue;
+R = data.Rtrue;
+% KF Tuning Knob
+Q = 80*Qtrue;
+
+% Inputs
+u_nom = zeros(2,length(tspan));
+u = zeros(2,length(tspan));
+
+% Getting nominal trajectory and measurements
+[~,x_nom] = ode45(@(tspan,var0) OrbitEOM(tspan,var0,mu),tspan,nom_var0,options);
+[output_var_nom,~] = MeasuredOutput(tspan',x_nom,RE,wE,false);
 
 %% CT Dynamics
 Abar = @(t) [0 1 0 0
@@ -55,129 +75,146 @@ delta_Xnom(t,i)/rhonom(t,i),... row 2 col 2
 delta_Ynom(t,i)/rhonom(t,i); % row 2 col 4
 -delta_Ynom(t,i)/rhonom(t,i)^2, 0, delta_Xnom(t,i)/rhonom(t,i)^2, 0]; % row 3
 
-%% Load in Data Logs and Define Inputs
-% Load in Data Logs and Q/R
-data = load('orbitdeterm_finalproj_KFdata.mat');
-Q = data.Qtrue;
-R = data.Rtrue;
-tvec_datalog = data.tvec;
-ydatalog = data.ydata;
-
-% ode45 call
-options = odeset('RelTol',1e-6,'AbsTol',1e-9);
-[t,state] = ode45(@(tspan,var) OrbitEOM(tspan,var,mu),tspan,pert_var0,options);
-[~,state_nom] = ode45(@(tspan,var0) OrbitEOM(tspan,var0,mu),tspan,nom_var0,options);
-
-% Get outputs
-[output_var,station_vis] = MeasuredOutput(t,state,RE,wE,true);
-[output_var_nom,~] = MeasuredOutput(t,state_nom,RE,wE,false);
-
-% Convert into cell array - filter nominal measurements to match NL idxs
-N = length(t);
-ycell_nom = cell(N,1);
-ycell_pert = cell(N,1);
-station_vis_cell = cell(N,1);
-for k = 1:N-1
-    yrowsKeep = ~any(isnan(output_var(:,k+1)),2); % rows that contain NO NaNs
-    stationrowsKeep = ~any(isnan(station_vis(:,k+1)),2); % rows that contain NO NaNs
-    y1 = output_var_nom(yrowsKeep,k+1);
-    y2 = output_var(yrowsKeep,k+1);
-    station = station_vis(stationrowsKeep,k+1);
-    ycell_nom{k+1} = y1;
-    ycell_pert{k+1} = y2;
-    station_vis_cell{k+1} = station;
-end
-
-y_pert = ycell_pert;
-y_nom = ycell_nom;
-station_vis = station_vis_cell;
-
-% Concatenate Outputs/Stations
-NL_outputs = [y_nom station_vis];
-
 Dbar = zeros(3,2);
 
-n_sims = 10;
-% epsilon_x = zeros(size(x,2),n_sims);
-for sim_num = 1:n_sims
-    %% CT -> DT and Simulate Perturbation Dynamics
-    % Initialize
-    delta_xk = zeros(4, N);
-    Pp0 = diag([10,0.1,10,0.1]);
-    delta_xk(:,1) = chol(Pp0,'lower') * randn(4,1);
-    delta_yk = cell(N,1);
-    Fk = zeros(4,4,N);
-    Hk = cell(N,1); 
-    yk = cell(N,1);
-    
-    % Process Noise Matrix
-    Gamma = Bbar; % w1,2 has same mapping as u1,2
-    Omegabar = delta_t.*Gamma;
-    Sw = chol(Q,'lower');
+Gamma = Bbar; % w1,2 has same mapping as u1,2
+Omegabar = delta_t.*Gamma;
+
+% Compute Fk once outside MC loop
+Fk = zeros(4,4,N);
+for k = 1:N-1
+    Fk(:,:,k) = eye(4) + delta_t.*Abar(tspan(k));
+end
+G = delta_t.*Bbar;
+
+%% MC
+num_sims = 10;
+epsilon_x = zeros(N,num_sims);
+epsilon_y = zeros(N,num_sims);
+for sim_num = 1:num_sims
+    %% Simulating nonlinear EOMs with perturbed initial condition and process noise
+    % ode45 call - pert w/ noise
+    x_true_pert = pert_var0;
+    x_pert_noisy = zeros(N,4);
+    x_pert_noisy(1,:) = x_true_pert';
+    e_x_EKF = zeros(N,4);
     for k = 1:N-1
-        % Build H at time t_k
-        num_vis = station_vis{k+1};
-        H = zeros(3*length(num_vis),4);
-        for i = 1:length(num_vis)
-            H(3*i-3+(1:3),:) = Cbar(tspan(k+1),num_vis(i));
-        end
-        Hk{k+1} = H;
-    
-        % Propagate perturbation to next step (except at the last time)
-        if k < N
-            Fk(:,:,k) = eye(4) + delta_t.*Abar(tspan(k));
-            G = delta_t.*Bbar;                  
-            delta_xk(:,k+1) = Fk(:,:,k)*delta_xk(:,k) + Omegabar*Sw*randn(2,1);
-        end
-    
-        % Linearized measurement at time t_k
-        delta_yk{k+1} = Hk{k+1}*delta_xk(:,k+1);
-        yk{k+1} = y_nom{k+1} + delta_yk{k+1};
+        % Propagate perturbed true state with noise using ode45
+        current_tspan = [tspan(k) tspan(k+1)];
+        [~,x_pert_k] = ode45(@(current_tspan,pert_var0)...
+            OrbitEOM(current_tspan,pert_var0,mu),current_tspan,x_true_pert,options);
+        xtrue_kp1 = x_pert_k(end,:); 
+        w_k = chol(Qtrue,"lower")*randn(2,1);
+        x_true_pert = xtrue_kp1' + Omegabar*w_k;
+        x_pert_noisy(k+1,:) = x_true_pert;
     end
-    
-    % Reconstruct the linearized full state
-    state_lin = state_nom + delta_xk.';
-    
-    % Concatanate Outputs/Stations
-    L_outputs = [yk,station_vis];
-    
-    % Inputs
-    u_nom = zeros(2,length(tspan));
-    u = zeros(2,length(tspan));
-    
-    %% Noise and Covariance
-    % Generate Noisy Measurements;
-    y_pert_noise = cell(N,1);
-    for k = 1:N-1
-        K = length(y_pert{k+1});
+
+    %% Simulating measurements for noisy NL simulation, adding measurment noise
+    % Also, building H matrices at each k time step
+    [station_meas,station_vis] = MeasuredOutput(tspan',x_pert_noisy,RE,wE,true);
+    % Convert into cell array - filter nominal measurements to match NL idxs
+    y_nom = cell(N,1);
+    y_pert_noisy = cell(N,1);
+    station_vis_cell = cell(N,1);
+
+    delta_xk = zeros(4, N);
+    delta_xk(:,1) = delta_x0;
+    Hk = cell(N,1);
+    p(1) = 0;
+    for k = 2:N
+        y_pert = station_meas(~any(isnan(station_meas(:,k)),2), k);
+        y_nom{k} = output_var_nom(~any(isnan(station_meas(:,k)),2), k);
+        K = length(y_pert);
         Sv = chol(kron(eye(K/3),R),'lower');
         qk = randn(K,1);
-        y_pert_noise{k+1} = y_pert{k+1} + Sv*qk;
+        y_pert_noisy{k} = y_pert + Sv*qk;
+        station_vis_cell{k} = station_vis(~any(isnan(station_vis(:,k)),2), k);
+
+        % Build H at time t_k
+        num_vis = station_vis_cell{k};
+        H = zeros(3*length(num_vis),4);
+        for i = 1:length(num_vis)
+            H(3*i-3+(1:3),:) = Cbar(tspan(k),num_vis(i));
+        end
+        Hk{k} = H;
+
+        % p keeps track of the number of available measurements at step k
+        p(k) = size(H,1);
     end
     
-    %% LKF
-    lkf_delta_x0 = zeros(4,1);
-    [x_LKF,y_LKF,Ppkp1_LKF] = LKF...
-        (Fk,G,Hk,Q,R,Omegabar,lkf_delta_x0,Pp0,state,u_nom,u,y_nom,y_pert_noise);
-    
-    LKF_outputs = [y_LKF station_vis];
-
-    epsilon_i_x = zeros(size(state_lin,2),1); % kx1
-    for k = 1:N 
-        x_k = state_lin(k,:)';
-        xhatp_k = x_LKF(:,k);
-        Pp_k = Ppkp1_LKF(:,:,k);
-        e_xk = x_k - xhatp_k;
-        epsilon_i_xk = e_xk'*Pp_k^-1*e_xk; % 1x1
-        epsilon_i_x(k,1) = epsilon_i_xk;
+    % Run LKF and calculate NEES and NIS
+    delta_x0_KF = zeros(4,1);
+    [x_LKF,y_LKF,Pmkp1_LKF,Ppkp1_LKF,innov_LKF,delta_x_LKF,Skp1] = LKF...
+        (Fk,G,Hk,Q,R,Omegabar,delta_x0_KF,Pp0,x_nom,u_nom,u,y_nom,y_pert_noisy);
+    % e_y_LKF = cell(N,1);
+    for k=2:N
+        % calculate epsilon_x for each time step k
+        e_x_LKF(k,:) = x_pert_noisy(k,:) - x_LKF(k,:);
+        epsilon_x_LKF(k,sim_num) = e_x_LKF(k,:) * (Ppkp1_LKF(:,:,k) \ e_x_LKF(k,:)');
+        % calculate epsilon_y for each time step k
+        epsilon_y_LKF(k,sim_num) = innov_LKF{k}' * (Skp1{k} \ innov_LKF{k});
     end
-    epsilon_x(:,sim_num) = epsilon_i_x;
 
+    % Run EKF and calculate NEES and NIS
+    [x_EKF, P_EKF, y_EKF, Sv_EKF] = EKF...
+        (Q, R, y_pert_noisy, tspan', mu, RE, wE, nom_var0, Pp0, station_vis_cell);
+    e_y_EKF = cell(N,1);
+    for k=2:N
+        % calculate epsilon_x for each time step k
+        e_x_EKF(k,:) = x_pert_noisy(k,:) - x_EKF(:,k)';
+        epsilon_x_EKF(k,sim_num) = e_x_EKF(k,:) * (P_EKF(:,:,k) \ e_x_EKF(k,:)');
+        % calculate epsilon_y for each time step k
+        e_y_EKF{k} = y_pert_noisy{k} - y_EKF{k};
+        epsilon_y_EKF(k,sim_num) = e_y_EKF{k}' * (Sv_EKF{k} \ e_y_EKF{k});
+    end
 end
+% Calculate NEES and NIS
+epsilonbar_x_LKF = (1/num_sims) .* sum(epsilon_x_LKF,2);
+epsilonbar_y_LKF = (1/num_sims) .* sum(epsilon_y_LKF,2);
 
-epsilonbar_x = sum(epsilon_x,2)/n_sims;
+% Getting upper and lower bounds for NEES and NIS
+alpha = 0.05;
+r1_NEES = chi2inv(alpha/2,num_sims*4)./num_sims;
+r2_NEES = chi2inv(1-alpha/2,num_sims*4)./num_sims;
+r1_NIS = chi2inv(alpha/2,num_sims*p)./num_sims;
+r2_NIS = chi2inv(1-alpha/2,num_sims*p)./num_sims;
+
+%% Plots of NEES and NIS for LKF and EKF
 figure;
-scatter(1:1401, epsilonbar_x, 'filled'); grid on;
+scatter(1:N, epsilonbar_x_LKF, 16, 'filled'); grid on; hold on;
 xlabel('Time Step k');
 ylabel('$\bar{\epsilon}_x$', 'Interpreter', 'latex');
 title('LKF NEES Test Results');
+xlim([1,N]);
+yline(r1_NEES,'--',"r1","Color","red");
+yline(r2_NEES,'--',"r2","Color","red");
+
+figure;
+scatter(1:N, epsilonbar_y_LKF, 16, 'filled'); grid on; hold on;
+xlabel('Time Step k');
+ylabel('$\bar{\epsilon}_y$', 'Interpreter', 'latex');
+title('LKF NIS Test Results');
+xlim([1,N]);
+plot(1:N,r1_NIS,'--',"Color","red");
+plot(1:N,r2_NIS,'--',"Color","red");
+
+epsilonbar_x_EKF = (1/num_sims) .* sum(epsilon_x_EKF,2);
+epsilonbar_y_EKF = (1/num_sims) .* sum(epsilon_y_EKF,2);
+
+figure;
+scatter(1:N, epsilonbar_x_EKF, 16, 'filled'); grid on; hold on;
+xlabel('Time Step k');
+ylabel('$\bar{\epsilon}_x$', 'Interpreter', 'latex');
+title('EKF NEES Test Results');
+xlim([1,N]);
+yline(r1_NEES,'--',"r1","Color","red");
+yline(r2_NEES,'--',"r2","Color","red");
+
+figure;
+scatter(1:N, epsilonbar_y_EKF, 16, 'filled'); grid on; hold on;
+xlabel('Time Step k');
+ylabel('$\bar{\epsilon}_y$', 'Interpreter', 'latex');
+title('EKF NIS Test Results');
+xlim([1,N]);
+% plot(1:N,r1_NIS,'--',"Color","red");
+% plot(1:N,r2_NIS,'--',"Color","red");
